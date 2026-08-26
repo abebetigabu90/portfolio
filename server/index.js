527fe9c90696a16da16236935db2b15426a3ab19
Abebe Tigabu <abebetigabu90@gmail.com>
2026-08-26 17:49:43 +0300

    final portfolio feature

--- server/index.js
+++ server/index.js
@@ -1,9 +1,27 @@
+// import app from "./app.js"
+// import connectDB from "./config/db.js"
+// const port = 5000
+// const startServer = async()=>{
+//     await connectDB()
+// app.listen(port,()=>{console.log(`server is running on port ${port}`)})
+
+// }
+// startServer()
+
+
+
+
 import app from "./app.js"
 import connectDB from "./config/db.js"
-const port = 5000
-const startServer = async()=>{
-    await connectDB()
-app.listen(port,()=>{console.log(`server is running on port ${port}`)})
 
+// Use Render's environment port, or fallback to 5000 for local testing
+const port = process.env.PORT || 5000
+
+const startServer = async () => {
+  await connectDB()
+  app.listen(port, () => {
+    console.log(`server is running on port ${port}`)
+  })
 }
+
 startServer()
